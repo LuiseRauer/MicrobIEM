60d1061e482aa915fb68b7bf7d47c5255e9bd40c
LuiseRauer <72738231+LuiseRauer@users.noreply.github.com>
2021-02-18 08:27:34 +0100

    Add package DT for visualisation of alpha diversity table

--- MicrobIEM/start_MicrobIEM.R
+++ MicrobIEM/start_MicrobIEM.R
@@ -1,5 +1,5 @@
 start_MicrobIEM <- function() {
-  # load required packages
+  # Load required packages
   if("shiny" %in% rownames(installed.packages()) == FALSE) 
     install.packages("shiny")
   library(shiny)
@@ -24,11 +24,13 @@ start_MicrobIEM <- function() {
   if("plotly" %in% rownames(installed.packages()) == FALSE) 
     install.packages("plotly")
   library(plotly)
+  if("DT" %in% rownames(installed.packages()) == FALSE) 
+    install.packages("DT")
+  library(DT)
   
-  
-  # load ui and server files
+  # Load ui and server files
   source("MicrobIEM/ui.R"); 
   source("MicrobIEM/server.R"); 
-  # start the shiny app
+  # Start the shiny app
   shinyApp(ui,server)
 }
